2b1e56afc8e59b47aebeb7b144c334b9bc687cd1
KambaleLuseko <46275850+KambaleLuseko@users.noreply.github.com>
2022-09-20 16:16:49 +0200

    Product transfer, equipment management integrated
    Transfer product from one store to another
    add equipement
    associate equipment to users

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e76570aa8d71cd315e1d8d35423b3c3d",
-"index.html": "2f9d52f1af9db19562ccc1ff1a268552",
-"/": "2f9d52f1af9db19562ccc1ff1a268552",
-"main.dart.js": "7143fb9ab363b41321058da0b225f64a",
+"index.html": "0c41ea08c5e3445172f2a136ad360b33",
+"/": "0c41ea08c5e3445172f2a136ad360b33",
+"main.dart.js": "0c473c70133d7f97d848b6e95150d0a8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "6a1ce2d667fea2f5429a4eef7093aa37",
 "assets/AssetManifest.json": "1f93c38f687d0d0d3f6b88e519b7e867",
-"assets/NOTICES": "e59694a743b2bf91232af373384f7ede",
+"assets/NOTICES": "fd36d8f963b8a87ca0044d806a686ab0",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
